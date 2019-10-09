11de00734615412338331c4e57a3125a3a57a9c1
Rey Diaz <reybdiazjr@gmail.com>
2019-10-08 22:06:13 -0400

    having id product name and price pop up in node.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,91 +1,101 @@
-var mysql = require ("mysql");
-var inquirer = require ("inquirer");
-var table = require ("cli-table2");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var table = require("cli-table2");
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "root",
     database: "bamazon_DB",
-    })
+})
 
-    connection.connect(function (err){
-        if (err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
 
-        displayProducts();
-        setTimeout(shop, 3000)
-    });
+    displayProducts();
+    setTimeout(shop, 3000)
+});
 
-    function displayProducts() {
-        connection.query("SELECT * FROM products", function(err, res){
-            console.log(`
+function displayProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        console.log(`
             ==========================================================================
-            ID | Product | Price
+                ID | Product | Price
             ==========================================================================`);
-            for (var i = 0; i < res.length; i++) {
-                console.log(`
+        for (var i = 0; i < res.length; i++) {
+            console.log(`
                 ${res[i].id} | ${res[i].product_name} | $${res[i].price}
             ==========================================================================`);
-            }
-        })
-    };
+        }
+    })
+};
 
-    function shop() {
-        connection.query("SELECT * FROM products", function (err,res){
-            inquirer
-                .prompt([{
-                    name: "choice",
-                    type: "list",
-                    choices: function() {
-                        var choiceArr = [];
-                        for (var i = 0; i < res.length; i++) {
-                            choiceArr.push(res[i].id.toString());
-                        }
-                        return choiceArr;
-                    },
-                    message: "What Item tickles your fancy? (choose by ID)"
+function shop() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        inquirer
+            .prompt([{
+                name: "choice",
+                type: "list",
+                choices: function () {
+                    var choiceArr = [];
+                    for (var i = 0; i < res.length; i++) {
+                        choiceArr.push(res[i].id.toString());
+                    }
+                    return choiceArr;
                 },
+                message: "What Item tickles your fancy? (choose by ID)"
+            },
             {
                 name: "quantity",
                 type: "list",
-                choices: ["1", "2","3", "4", "5"],
+                choices: ["1", "2", "3", "4", "5"],
                 message: "How many would you like to purchase?"
             }
-        ])
-        .then(function(answer){
-            var chosenItems;
-            for (var i = 0; i < res.length; i++) {
-                if (res[i].id === parseInt(answer.choice)){
-                    chosenItem = res[i];
-                }
-            }
-
-        var  totalPaid = chosenItems.price * answer.quantity;
-
-        if(chosenItems.stock_quantity <= parseInt(answer.quantity)) {
-            connection.query(
-                "Update products Set ? Where?",
-                [
-                    {
-                        stock_quantity: chosenItems.stock_quantity - answer.quantity
-                    },
-                    {
-                        id: chosenItems.id
+            ])
+            .then(function (answer) {
+                var chosenItems;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].id === parseInt(answer.choice)) {
+                        chosenItem = res[i];
                     }
-                ],
-                
-            )
-        }
-        })
-        })
-    }
+                }
+
+                var totalPaid = chosenItems.price * answer.quantity;
+
+                if (chosenItems.stock_quantity <= parseInt(answer.quantity)) {
+                    connection.query(
+                        "Update products Set ? Where?",
+                        [
+                            {
+                                stock_quantity: chosenItems.stock_quantity - answer.quantity
+                            },
+                            {
+                                id: chosenItems.id
+                            }
+                        ],
+                        function (error) {
+                            if (err) throw err;
+                            console.log("Purchase Made! Damn Bro you spent a total of $" + totalPaid);
+                            setTimeout(displayProducts, 3000);
+                            setTimeout(shop, 5000);
+                        }
+                    )
+                }
+                else {
+                    console.log("Sorry we are out of stock of that item. AND DONT ASK ME TO CHECK IN THE BACK!");
+                    setTimeout(displayProducts, 3000);
+                    setTimeout(shop, 5000);
+                }
+            });
+
+    })
+}
 
 
-    
